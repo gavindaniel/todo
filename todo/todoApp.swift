35fd56912b0dbc49813412b2576ce2286d6c47d9
Gavin Daniel <accts.gd@gmail.com>
2022-09-27 11:16:15 -0700

    init

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Gavin Daniel on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
